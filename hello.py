419f7dbb4f330af8fe36883dacf8cb42a21b7d00
Dima9460$ <fordima2102@gmail.com>
2023-04-01 12:41:05 -0500

    Innitial commit

---
+++ hello.py
@@ -0,0 +1,45 @@
+
+print("Hello World")
+
+#Mac and Windows are different operating systems
+#THey will be using different types of terminals
+#(Terminal) _ Where we see the output of our codes 
+# in the VS Code editor.
+# Since MAC and WIndows are different O/S the terminals
+# they are using are different
+
+# By commenting the lines we can avoid the lines 
+# being included in the running process.
+# add a # symbol to comment the lines.
+# To comment multiple lines in python
+# highligh the lines to be commented then press 
+# command + / (MAC) or control + / (windows)
+
+print('Hello World')
+
+# There is no difference between enclosing the 
+# text with single or double quotes in python.
+
+# I want to print the sentence
+# It's a lovely day
+print("It's a lovely day")
+
+# How can you print the sentence below without changing
+# He said "It's a lovely day to learn python"
+# Escape Character
+# There are special characters used to insert specific characters in 
+# a text .  We use escape characters are preceded by a backslash \ 
+# to include them in the text.
+
+
+print('He said "It\'s a lovely day to learn python"')
+
+print("He said \"It\'s a lovely day to learn python\"")
+
+# How can you print \python\ ?
+
+print('\python\\')
+
+print("\\python\\")
+
+
